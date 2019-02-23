5506c2ccb0efd31536e37e58843195697bb0991a
Jenny Eddie <aigagror@gmail.com>
2019-02-23 23:43:24 +0000

    improved trainig code

--- train.py
+++ train.py
@@ -2,19 +2,20 @@ from models.densenet import DenseNet
 from models.vector_quantization import VQModel
 import argparse
 
-parser = argparse.ArgumentParser(description='Train the model')
-parser.add_argument('--epochs', '-e', type=int, help='Specify number of epochs', default=1)
-parser.add_argument('--version_name', '-v', type=str, default='default', help='Specify name of version')
-
 available_models = list(map(lambda model: model.__name__, [DenseNet, VQModel]))
-parser.add_argument('--model', '-m', type=str, help='Specify name of model: {}'.format(available_models))
 
+# Parse arguments
+parser = argparse.ArgumentParser(description='Train the model')
+parser.add_argument('--epochs', '-e', type=int, help='Number of epochs', default=1)
+parser.add_argument('--version_name', '-v', type=str, default='default', help='Name of version')
+parser.add_argument('--model', '-m', type=str, help='Choose model: {}'.format(available_models))
 args = parser.parse_args()
 
 epochs = args.epochs
 vers_name = args.version_name
 model_name = args.model
 
+# Do stuff
 Model = eval(model_name)
 
 model = Model()
