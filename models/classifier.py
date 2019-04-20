81f7c485f35c23c9936adf418bf5457b7fb01b40
Eddie Huang <aigagror@gmail.com>
2019-04-20 16:00:52 -0500

    more complicated classifier

--- models/classifier.py
+++ models/classifier.py
@@ -2,16 +2,29 @@ from torch import nn
 
 class Classifier(nn.Module):
     
-    def __init__(self, input_size, num_classes):
+    def __init__(self, num_classes):
         super().__init__()
         
-        self.input_size = input_size
-        
-        self.main = nn.Sequential(
-            nn.Linear(3 * (input_size ** 2), num_classes),
-            nn.Softmax(1)
+        self.convs = nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=(5,5), stride=2, padding=2, bias=False),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.Conv2d(16, 32, kernel_size=(5,5), stride=2, padding=2, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=(5,5), stride=2, padding=2, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, kernel_size=(5,5), stride=2, padding=2, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Conv2d(128, 256, kernel_size=(5,5), stride=2, padding=2, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
         )
+        self.linear = nn.Linear(256 * 8 * 8, num_classes)
         
     def forward(self, img):
-        out = self.main(img.view(-1, 3 * (self.input_size ** 2)))
+        out = self.convs(img)
+        out = self.linear(out.view(img.shape[0], -1))
         return out
