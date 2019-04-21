1abeee600ebc27073491b2a87408c3443d0968a4
Jenny Eddie <aigagror@gmail.com>
2019-04-21 19:16:35 +0000

    binary ava. main notebook loads and saves models

--- data/AVA_dataset/dataset.py
+++ data/AVA_dataset/dataset.py
@@ -29,3 +29,32 @@ class AVA(torch.utils.data.Dataset):
         
         return img, prob_distr
         
+class BinaryAVA(torch.utils.data.Dataset):
+    def __init__(self, aesthetic_path, transform=None):
+        super().__init__()
+        self.df = pd.read_csv('data/AVA_dataset/proc_AVA.csv')
+        self.aesth_df = pd.read_csv(aesthetic_path, names=['Image ID'])
+        self.df = self.df.merge(self.aesth_df)
+        
+        beautiful_df = self.df.nlargest(top_10_perc_size, 'Mean Score')
+        beautiful_df['Beautiful'] = 1
+        
+        ugly_df = self.df.nsmallest(top_10_perc_size, 'Mean Score')
+        ugly_df['Beautiful'] = 0
+        
+        self.df = pd.concat([beautiful_df, ugly_df], ignore_index=True)
+        
+        self.transform = transform
+        
+    def __len__(self):
+        return len(self.df)
+    
+    def __getitem__(self, idx):
+        item = self.df.iloc[idx]
+        img_id = item['Image ID']
+        img = Image.open('data/AVA_dataset/images/{}.jpg'.format(img_id)).convert('RGB')
+        
+        if self.transform is not None:
+            img = self.transform(img)
+        
+        return img, item['Beautiful']
