b2534c8dbcaf946c1c10cd5596e8a7cab066814c
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-05-22 21:16:56 +0000

    added figures folder

--- main.py
+++ main.py
@@ -35,7 +35,6 @@ def rate(img_path):
             transforms.Resize(224),
             transforms.CenterCrop(224),
             transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
     img = transform(img)
 
