cdc43e8d57122923128ddde552394f9f04672d02
Dell Du <18588220928@163.com>
2021-02-03 23:34:21 +0800

    Support onnx.

--- main.py
+++ main.py
@@ -1,33 +1,42 @@
 import argparse
 import os
 import glob
-import numpy as np
 import torch
 from PIL import Image
-from torch import nn
+# from torch import nn
 from torchvision import models, transforms
+from model import model_setenv, get_model, model_load, model_device
+import pdb
 
 def rate(img_path):
     """
     Returns: Scores, mean, std
     """
-    # Number of classes in the dataset
-    num_classes = 10
+    # # Number of classes in the dataset
+    # num_classes = 10
 
-    model_ft = models.densenet121(pretrained=False)
-    num_ftrs = model_ft.classifier.in_features
-    model_ft.classifier = nn.Sequential(
-        nn.Linear(num_ftrs, num_classes),
-        nn.Softmax(1)
-    )
+    # model_ft = models.densenet121(pretrained=False)
+    # num_ftrs = model_ft.classifier.in_features
+    # model_ft.classifier = nn.Sequential(
+    #     nn.Linear(num_ftrs, num_classes),
+    #     nn.Softmax(1)
+    # )
 
-    # Weight Path
-    weight_path = 'weights/dense121_all.pt'
+    # # Weight Path
+    # weight_path = 'weights/dense121_all.pt'
 
-    # Load weights
-    assert os.path.exists(weight_path)
-    model_ft.load_state_dict(torch.load(weight_path))
-    model_ft.eval()
+    # # Load weights
+    # assert os.path.exists(weight_path)
+    # model_ft.load_state_dict(torch.load(weight_path))
+    # model_ft.eval()
+
+    model_setenv()
+    device = model_device()
+    model = get_model()
+    model_name = 'models/ImageNima.pth'
+    model_load(model, model_name)
+    model = model.to(device)
+    model.eval()
 
     image_filenames = sorted(glob.glob(img_path))
 
@@ -36,14 +45,15 @@ def rate(img_path):
         transforms.CenterCrop(224),
         transforms.ToTensor(),
     ])
+    weighted_votes = torch.arange(10, dtype=torch.float) + 1
+    weighted_votes = weighted_votes.to(device)
 
     for index, filename in enumerate(image_filenames):
         img = Image.open(filename).convert('RGB')
-        img = transform(img)
+        img = transform(img).to(device)
 
         with torch.no_grad():
-            scores = model_ft(img.view(1, 3, 224, 224))
-            weighted_votes = torch.arange(10, dtype=torch.float) + 1
+            scores = model(img.view(1, 3, 224, 224))
             mean = torch.matmul(scores, weighted_votes)
             std = torch.sqrt((scores * torch.pow((weighted_votes - mean.view(-1, 1)), 2)).sum(dim=1))
 
@@ -54,6 +64,6 @@ def rate(img_path):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Use DenseNet NIMA')
 
-    parser.add_argument('--img_path', type=str, default="images/*", help='Path to input image')
+    parser.add_argument('--input', type=str, default="images/*", help='Path to input image')
     args = parser.parse_args()
-    rate(args.img_path)
+    rate(args.input)
---
+++ model.py
@@ -0,0 +1,171 @@
+"""Create model."""
+# coding=utf-8
+#
+# /************************************************************************************
+# ***
+# ***    Copyright Dell 2020, All Rights Reserved.
+# ***
+# ***    File Author: Dell, 2020年 09月 09日 星期三 23:56:45 CST
+# ***
+# ************************************************************************************/
+#
+
+import os
+import pdb
+
+import torch
+import torch.nn as nn
+from torchvision import models, transforms
+
+def model_load(model, path):
+    """Load model."""
+    if not os.path.exists(path):
+        print("Model '{}' does not exist.".format(path))
+        return
+
+    state_dict = torch.load(path, map_location=lambda storage, loc: storage)
+    target_state_dict = model.state_dict()
+    for n, p in state_dict.items():
+        if n in target_state_dict.keys():
+            target_state_dict[n].copy_(p)
+        else:
+            raise KeyError(n)
+
+
+def model_save(model, path):
+    """Save model."""
+    torch.save(model.state_dict(), path)
+
+
+def export_onnx_model():
+    """Export onnx model."""
+    import onnx
+    import onnxruntime
+    from onnx import optimizer
+    import numpy as np
+
+    onnx_init_running = True
+    dummy_input = torch.randn(1, 3, 224, 224)
+    onnx_file_name = "output/image_nima.onnx"
+    if not os.path.exists("output"):
+        os.makedirs("output")  
+
+    # 1. Create and load model.
+    model_setenv()
+    torch_model = get_model()
+    model_name = 'models/ImageNima.pth'
+    model_load(torch_model, model_name)
+
+    torch_model.eval()
+
+    with torch.no_grad():
+        torch_output = torch_model(dummy_input)
+
+    # 2. Model export
+    print("Export model ...")
+
+    if onnx_init_running:
+        input_names = ["input"]
+        output_names = ["output"]
+
+        torch.onnx.export(torch_model, dummy_input, onnx_file_name,
+                          input_names=input_names,
+                          output_names=output_names,
+                          verbose=True,
+                          opset_version=11,
+                          keep_initializers_as_inputs=False,
+                          export_params=True)
+
+    # 3. Optimize model
+    print('Checking model ...')
+    onnx_model = onnx.load(onnx_file_name)
+    onnx.checker.check_model(onnx_model)
+    # https://github.com/onnx/optimizer
+
+    # 4， Runtime checking ...
+    onnxruntime_engine = onnxruntime.InferenceSession(onnx_file_name)
+
+    def to_numpy(tensor):
+        return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+
+    onnxruntime_inputs = {onnxruntime_engine.get_inputs()[0].name: to_numpy(dummy_input)}
+    onnxruntime_outputs = onnxruntime_engine.run(None, onnxruntime_inputs)
+
+    np.testing.assert_allclose(to_numpy(torch_output), onnxruntime_outputs[0], rtol=1e-03, atol=1e-03)
+
+    print("Exported model has been tested with ONNXRuntime, the result looks good!")
+
+    # 5. Visual model
+    # python -c "import netron; netron.start('output/image_fill.onnx')"
+
+
+def export_torch_model():
+    """Export torch model."""
+
+    script_file = "output/image_nima.pt"
+    weight_file = "models/ImageNima.pth"
+    if not os.path.exists("output"):
+        os.makedirs("output")    
+
+    # 1. Load model
+    print("Loading model ...")
+    model = get_model()
+    model_load(model, weight_file)
+    model.eval()
+
+    # 2. Model export
+    print("Export model ...")
+    dummy_input = torch.randn(1, 3, 224, 224)
+    traced_script_module = torch.jit.trace(model, dummy_input)
+    traced_script_module.save(script_file)
+
+
+def get_model():
+    """Create model."""
+    model_setenv()
+    num_classes = 10
+    model = models.densenet121(pretrained=False)
+    num_ftrs = model.classifier.in_features
+    model.classifier = nn.Sequential(
+        nn.Linear(num_ftrs, num_classes),
+        nn.Softmax(1)
+    )
+    return model
+
+
+def model_device():
+    """First call model_setenv. """
+    return torch.device(os.environ["DEVICE"])
+
+
+def model_setenv():
+    """Setup environ  ..."""
+
+    # random init ...
+    import random
+    random.seed(42)
+    torch.manual_seed(42)
+
+    # Set default device to avoid exceptions
+    if os.environ.get("DEVICE") != "cuda" and os.environ.get("DEVICE") != "cpu":
+        os.environ["DEVICE"] = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    if os.environ["DEVICE"] == 'cuda':
+        torch.backends.cudnn.enabled = True
+        torch.backends.cudnn.benchmark = True
+
+    print("Running Environment:")
+    print("----------------------------------------------")
+    print("  PWD: ", os.environ["PWD"])
+    print("  DEVICE: ", os.environ["DEVICE"])
+
+
+if __name__ == '__main__':
+    """Test model ..."""
+
+    model = get_model()
+    print(model)
+
+    export_torch_model()
+    export_onnx_model()
+
